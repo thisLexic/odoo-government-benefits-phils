1014e9891e894c5c5d98610d48dfe06a932cd245
thisLexic <benvillabroza1999@gmail.com>
2020-10-10 18:28:00 +0800

    connect hr.employee to gov_bene_phils

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -6,3 +6,7 @@ class Employee(models.Model):
 
     emp_sss_detl_ids = fields.One2many('gov_bene_phils.sss_employee_details', 'emp_id', string='SSS Benefits')
     emp_sss_num = fields.Char(string="SSS Number")
+    emp_philhealth_detl_ids = fields.One2many('gov_bene_phils.philhealth_employee_details', 'emp_id', string='PhilHealth Benefits')
+    emp_philhealth_num = fields.Char(string="PhilHealth Number")
+    emp_pagibig_detl_ids = fields.One2many('gov_bene_phils.pagibig_employee_details', 'emp_id', string='Pag-Ibig Benefits')
+    emp_pagibig_num = fields.Char(string="Pag-Ibig Number")
