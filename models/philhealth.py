c7a39843ebf6d93409f0f6e99b8002d2bd71447b
thisLexic <benvillabroza1999@gmail.com>
2020-10-10 15:30:23 +0800

    finish Pagibig

--- models/philhealth.py
+++ models/philhealth.py
@@ -43,8 +43,8 @@ class Philhealth(models.Model):
     date_contrib_end = fields.Date(string="End Date")
     ref = fields.Char(string="Reference")
     transaction_number = fields.Char(string="Trans Num")
-    # image_ids = fields.One2many('gov_bene_phils.sss_image', 'sss_id', string='Images')
-    # emp_detl_ids = fields.One2many('gov_bene_phils.sss_employee_details', 'sss_id', string='Employee Benefits')
+    image_ids = fields.One2many('gov_bene_phils.philhealth_image', 'philhealth_id', string='Images')
+    emp_detl_ids = fields.One2many('gov_bene_phils.philhealth_employee_details', 'philhealth_id', string='Employee Benefits')
     company_id = fields.Many2one('res.company', string="Company/Employer")
     date_month = fields.Char(string="Month", compute="_get_month", search="_search_month", store=True)
     date_year = fields.Char(string="Year", compute="_get_year", search="_search_year", store=True)
@@ -54,3 +54,46 @@ class Philhealth(models.Model):
     check_date = fields.Date(string="Check Date")
     check_bank = fields.Char(string="Bank")
     check_branch = fields.Char(string="Branch Name")
+
+class Image(models.Model):
+    _name = 'gov_bene_phils.philhealth_image'
+    _description = 'Contains images of PhilHealth details'
+
+
+    @api.depends('image')
+    def _get_preview(self):
+        self.preview = self.image
+
+
+    def _get_image_html(self):
+        for elem in self:
+            attachment = self.env['ir.attachment'].search(
+                [('res_model', '=', 'gov_bene_phils.philhealth_image'),
+                ('res_id', '=', self.id)], limit=1
+                )
+            image_url = "/web/image/ir.attachment/%s/datas" % attachment.id
+            elem.image_loc = '<img src="%s"/>' % image_url
+
+
+    philhealth_id = fields.Many2one('gov_bene_phils.philhealth', string="PhilHealth Payment")
+    label = fields.Char(string="Label")
+    image = fields.Binary(string="Image")
+    preview = fields.Binary(string="Preview", compute="_get_preview")
+    image_loc = fields.Html(string='Preview 2', compute="_get_image_html")
+
+
+class Sss_Employee_Details(models.Model):
+    _name = 'gov_bene_phils.philhealth_employee_details'
+    _description = 'Contains employee payment details of PhilHealth transaction'
+
+
+    def _default_currency_id(self):
+         return self.env['res.currency'].search([('name', '=', 'PHP')], limit=1).id
+
+
+    philhealth_id = fields.Many2one('gov_bene_phils.philhealth', string="PhilHealth Payment")
+    currency_id = fields.Many2one('res.currency', string="Currency", default=_default_currency_id)
+    emp_id = fields.Many2one('hr.employee', string="Employee")
+    emp_contrib = fields.Monetary(string="Employee Contribution")
+    comp_contrib = fields.Monetary(string="Owner Contribution")
+    ec_contrib = fields.Monetary(string="EC Contribution")
