c1a671d0092601b6707a1779ce35d96e09744289
thisLexic <benvillabroza1999@gmail.com>
2020-10-10 17:12:22 +0800

    added company number for philhealth and pagibig in res_company

--- models/res_company.py
+++ models/res_company.py
@@ -5,3 +5,5 @@ class Company(models.Model):
     _inherit = "res.company"
 
     com_sss_num = fields.Char(string="SSS Number")
+    com_philhealth_num = fields.Char(string="PhilHealth Number")
+    com_pagibig_num = fields.Char(string="Pag-Ibig Number")
